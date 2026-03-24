cf5586e2b08c5707aed8eddc404914e8b76c640a
RustySyrup <alexisgagnon81@gmail.com>
2026-03-23 17:20:54 -0700

    sure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
-import earth from "./earth.svg";
-import moon from "./moon.svg";
-import light from "./light.svg";
-import moonlight from "./moonlight.svg";
+import React, { useState } from "react";
 import EarthAndMoon from "./EarthAndMoon";
+import Illustration from "./Illustration";
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState("home");
+
   return (
     <div className="App">
       <header className="header">
@@ -14,25 +13,35 @@ function App() {
           <a
             className="nava"
             href="https://science.nasa.gov/earth/"
-            target="blank"
+            target="_blank"
+            rel="noreferrer"
           >
             Earth
           </a>
+
+          <button
+            className="button"
+            onClick={() => setPage("illustration")}
+            type="button"
+          >
+            illustrations
+          </button>
+
           <a
             className="nava"
             href="https://science.nasa.gov/moon/"
-            target="blank"
+            target="_blank"
+            rel="noreferrer"
           >
             Moon
           </a>
         </nav>
       </header>
 
-      <header className="App-header">
-        <EarthAndMoon />
-      </header>
-
-      <body className="body"></body>
+      <main className="App-header">
+        {page === "home" && <EarthAndMoon />}
+        {page === "illustration" && <Illustration />}
+      </main>
 
       <aside className="aside">
         <p>this is aside</p>
@@ -48,3 +57,60 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState } from "react";
+// import logo from "./logo.svg";
+// import earth from "./earth.svg";
+// import moon from "./moon.svg";
+// import light from "./light.svg";
+// import moonlight from "./moonlight.svg";
+// import EarthAndMoon from "./EarthAndMoon";
+// import Illustration from "./Illustration";
+// import "./App.css";
+
+// function App() {
+//   const [page, setPage] = useState("home");
+//   return (
+//     <div className="App">
+//       <header className="header">
+//         <nav className="nav">
+//           <a
+//             className="nava"
+//             href="https://science.nasa.gov/earth/"
+//             target="_blank"
+//           >
+//             Earth
+//           </a>
+//           <button className="nava" onClick={() => setPage("illustrations")}>
+//             illustrations
+//           </button>
+//           <a
+//             className="nava"
+//             href="https://science.nasa.gov/moon/"
+//             target="_blank"
+//           >
+//             Moon
+//           </a>
+//         </nav>
+//       </header>
+
+//       <header className="App-header">
+//         <EarthAndMoon />
+//       </header>
+
+//       <body className="body"></body>
+
+//       <aside className="aside">
+//         <p>this is aside</p>
+//       </aside>
+
+//       <footer className="footer">
+//         <p>
+//           <span className="pfoot">2026 RustySyrup</span>
+//         </p>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Illustration.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Illustration.css";
+
+function Illustration() {
+  return (
+    <div className="illheader">
+      <p>second page</p>
+    </div>
+  );
+}
+
+export default Illustration;
+
+// import React, { useState } from "react";
+// import "./Illustration.css";
+
+// function Illustration() {
+//   const [page, setPage] = useState("illustration");
+//   return (
+//     <header className="illheader">
+//       <p>second page</p>
+//     </header>
+//   );
+// }
+
+// export default Illustration;
